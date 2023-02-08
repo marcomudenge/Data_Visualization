5b534397799ce0b5f0448c002bf2bba5671faecd
marcomudenge <44266914+marcomudenge@users.noreply.github.com>
2023-02-08 08:35:51 +0000

    Added noyng's preprocess.py file

--- src/preprocess.py
+++ src/preprocess.py
@@ -2,6 +2,7 @@
     Contains some functions to preprocess the data used in the visualisation.
 '''
 import pandas as pd
+import numpy as np 
 
 
 def convert_dates(dataframe):
@@ -13,7 +14,8 @@ def convert_dates(dataframe):
         Returns:
             The processed dataframe with datetime-formatted dates.
     '''
-    # TODO : Convert dates
+
+    dataframe['Date_Plantation'] = pd.to_datetime(dataframe['Date_Plantation'], format='%Y-%m-%d')
     return dataframe
 
 
@@ -29,7 +31,9 @@ def filter_years(dataframe, start, end):
         Returns:
             The dataframe filtered by date.
     '''
-    # TODO : Filter by dates
+    
+    dataframe = dataframe[(dataframe['Date_Plantation'] > pd.to_datetime(str(start))) 
+                & (dataframe['Date_Plantation'] < pd.to_datetime(str(end + 1)))]
     return dataframe
 
 
@@ -46,8 +50,14 @@ def summarize_yearly_counts(dataframe):
             containing the counts of planted
             trees for each neighborhood each year.
     '''
-    # TODO : Summarize df
-    return None
+    # Sort values by area and date
+    dataframe = dataframe.sort_values(by = ['Arrond_Nom', 'Date_Plantation'], ascending = [True, True])
+    
+    # Drop columns not needed for dataviz
+    dataframe = dataframe.drop(['Arrond', 'Longitude', 'Latitude'], axis=1)
+    dataframe['Date_Plantation'] = dataframe['Date_Plantation'].dt.strftime('%Y')
+    dataframe = dataframe[['Arrond_Nom', 'Date_Plantation']].value_counts().reset_index(name='count')
+    return dataframe
 
 
 def restructure_df(yearly_df):
@@ -68,8 +78,16 @@ def restructure_df(yearly_df):
         Returns:
             The restructured dataframe
     '''
-    # TODO : Restructure df and fill empty cells with 0
-    return None
+    # Get np arrays of unique areas and years. Used for index names and columns headers
+    years = np.sort(yearly_df['Date_Plantation'].unique())
+    areas = np.sort(yearly_df['Arrond_Nom'].unique())
+    frame = pd.DataFrame(columns=years, index=areas)
+    # Match all area/year in yearly_df to new frame
+    for _, row in yearly_df.iterrows():
+        frame.loc[[row['Arrond_Nom']],[row['Date_Plantation']]] = row['count']
+    # Replace nan with zeros 
+    frame = frame.fillna(0)
+    return frame
 
 
 def get_daily_info(dataframe, arrond, year):
@@ -86,5 +104,17 @@ def get_daily_info(dataframe, arrond, year):
             The daily tree count data for that
             neighborhood and year.
     '''
-    # TODO : Get daily tree count data and return
-    return None
+    # Filter frame on arrond and year
+    dataframe = dataframe[(dataframe['Arrond_Nom']==arrond) & (dataframe['Date_Plantation'].dt.year == year)]
+    
+    # Drop unnecessary columns
+    dataframe = dataframe.drop(['Arrond', 'Longitude', 'Latitude'], axis=1)
+    
+    # Count number of trees for every date, sort by date
+    dataframe = dataframe[['Date_Plantation']].value_counts().reset_index(name='Counts').sort_values('Date_Plantation')
+    
+    # Fill frame with zeros for dates that are not already present. 
+    # (Source: https://skipperkongen.dk/2018/11/26/how-to-fill-missing-dates-in-pandas/)
+    date_range = pd.date_range(start=dataframe.Date_Plantation.min(), end=dataframe.Date_Plantation.max())
+    dataframe = dataframe.set_index('Date_Plantation').reindex(date_range).fillna(0).rename_axis('Date_Plantation').reset_index()
+    return dataframe
